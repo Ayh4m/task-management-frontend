279813a5ebeaa6cb8c29881d6ec901acd5040963
Ayh4m <Ayh4m@users.noreply.github.com>
2021-08-16 09:03:25 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.fc5fcc7f220aef671fb125eb196c64f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff635293e62d927a99db866dd4d09993",
+    "url": "https://Ayh4m.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "8a312d7749daaeb077c8",
+    "url": "https://Ayh4m.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "83a5038d2ae7230d4582",
+    "url": "https://Ayh4m.github.io/task-management-frontend/static/js/2.60d48659.chunk.js"
+  },
+  {
+    "revision": "8a312d7749daaeb077c8",
+    "url": "https://Ayh4m.github.io/task-management-frontend/static/js/main.3521abda.chunk.js"
+  },
+  {
+    "revision": "43e94c24a9066b9ba0d3",
+    "url": "https://Ayh4m.github.io/task-management-frontend/static/js/runtime~main.c4fbf6a2.js"
+  }
+]);
